f930d9f2a7355d6eb5a88cc341c9267b4fc7ec8f
TinySpins <100190778+TinySpins@users.noreply.github.com>
2023-04-21 09:46:21 +0200

    Updated

--- contour42_view.m
+++ contour42_view.m
@@ -2,19 +2,23 @@
 
 % Companion script to contour42.m that plots the dicom files in a series
 % and overlays masks of the extracted cvi42 contours, using imtool3D.m
+
+% Currently, this tool does not sort dicom images according to slice
+% dimension, but only by Instance Number dicom tag. This means that slices 
+% and their masks are plotted along the 'Slice' dimension in imtool3D only.
     
 % House cleaning
 clear all; close all; clc;
 
 % Get path to a contour folder:
-disp('// Please select the directory of the contour file you want to display')
+disp('// Please select the directory of the Contour file you want to display')
 disp('// The corresponding Dicom images will load automatically')
 path = GetPath;
 
 % Load the contour file
 load([path.full '/' path.folder ' [Contour Masks].mat'])
 
-% Load the Dicom series (use dicomreadVolume instead!!!!)
+% Load the Dicom series
 images = dir([dicom_path '/*.dcm']);
 images = natsortfiles(images);
 for k = 1:length(images)
@@ -23,19 +27,32 @@ end
 
 % Prepare masks for plotting
 names = fieldnames(mask);
-masks = zeros(size(mask.(names{1})));
+masks = zeros(size(I));
 for k = 1:length(fieldnames(mask))
-    current_mask = mask.(names{k}) .* k;
-    masks = masks + current_mask;
+    try % exclude Points
+        current_mask = mask.(names{k}) .* k;
+        masks = masks + current_mask;
+    catch me % handle Points
+        task_name = fieldnames(mask);
+        if strcmp(class(mask.(names{k})),'double') && contains(task_name{k},'Point')
+            % turn point into mask and add
+            for kk = 1:size(mask.(names{k}),3)
+                ind = sub2ind(size(I,1,2),...
+                                round(mask.(names{k})(1,2,kk)),...
+                                       round(mask.(names{k})(1,1,kk)));
+                blank = zeros(size(I,1,2));
+                blank(ind) = 10;
+                point_mask(:,:,kk) = blank;
+            end
+            masks = masks + point_mask;
+        end
+    end
 end
 
 % Display images and contour masks in Imtool3D
 tool = imtool3D(I);
 tool.setMask(masks)
-tool.setAlpha(.1)
-tool.setMaskColor(jet(max(masks(:))+1))
+tool.setAlpha(.3)
 
 % Clear command window
 clc;
-
-
